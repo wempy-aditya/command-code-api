68ded3956c610d16ef65cc1730af44e5b40a5995
Wempy Aditya <adityawempy@gmail.com>
2026-06-09 16:10:51 +0700

    update redis port from 6379 to 6378 in config.py

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ STORAGE_DIR = BASE_DIR / "storage"
 WORKSPACE_DIR = BASE_DIR / "workspaces"
 
 REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_PORT = 6378
 
 CMD_BINARY = "/www/server/nodejs/v20.13.1/bin/cmd"
